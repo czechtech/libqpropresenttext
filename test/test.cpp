8f60d00ebe75f8f47cc6b39836f6d52adda1197d
czechtech <Email>
2014-04-22 19:25:18 -0400

    More robust testing.

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <csignal>
 #include <QCoreApplication>
+#include <QTimer>
 #include "qpropresenttext.h"
 
 using namespace std;
 
 QCoreApplication *a;
+QProPresentText *p;
 
 void signalHandler(int signum) { cerr << "Terminating\n"; a->exit(0); }
 
 class TestClass: public QObject
 {
     Q_OBJECT
+public:
+    QString host;
 public slots:
+	void makeConnection() { if(p->connectionState() == QAbstractSocket::UnconnectedState) { p->connectToProPresent(host); } }
 	void onPPConnected() { qDebug() << "ProPresentText Connected"; }
 	void onPPDisconnected() { qDebug() << "ProPresentText Disconnected"; }
 	void onPPNextSlide(QString text) { qDebug() << "Next Slide: " << text; }
@@ -30,13 +35,21 @@ int main(int argc, char *argv[])
   a = new QCoreApplication(argc, argv);
   signal(SIGINT,  signalHandler); signal(SIGTERM, signalHandler);
 
+  p = new QProPresentText(NULL);
+
   TestClass t;
-  QProPresentText p(NULL);
-  p.connectToProPresent(argv[1], 45578, "password");
+  t.host = argv[1];
+  t.makeConnection();
+  
   QObject::connect(p, SIGNAL(connected()), &t, SLOT(onPPConnected()));
   QObject::connect(p, SIGNAL(disconnected()), &t, SLOT(onPPDisconnected()));
   QObject::connect(p, SIGNAL(nextSlideChanged(QString)), &t, SLOT(onPPNextSlide(QString)));
   QObject::connect(p, SIGNAL(currentSlideChanged(QString)), &t, SLOT(onPPCurrentSlide(QString)));
+
+  // Maintain the connection:
+  QTimer timer;
+  QObject::connect(&timer, SIGNAL(timeout()), &t, SLOT(makeConnection()));  
+  timer.start(1000);
   
   return a->exec();
 }
